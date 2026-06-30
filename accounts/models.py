8e1bb2f6183ce0cb16c26f269688b8aebe5327f6
aiswarya <aiswaryakb400@gmail.com>
2026-06-30 11:51:57 +0530

    created customuser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class Customuser(AbstractUser):
+
+    ROLE_CHOICES =(
+        ("Admin","Admin"),
+        ("Employer","Employer"),
+        ("Candidate","Candidate"),
+    )
+
+    username=None
+    email=models.EmailField(unique=True)
+    phone=models.CharField(max_length=15)
+    role=models.CharField(max_length=20,choices=ROLE_CHOICES)
+    is_verified=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD="email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
--- authproject/settings.py
+++ authproject/settings.py
@@ -117,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#auth_user model created
+
+AUTH_USER_MODEL = "accounts.customuser"
